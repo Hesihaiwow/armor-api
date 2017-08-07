71ac9685a4029e2556f5134a02fe105be01126e4
ChenPeng <diaozhatian666@gmail.com>
2017-08-07 15:40:08 +0800

    提交代码

--- armor-api/src/main/java/com/zhixinhuixue/armor/dao/IZSYTaskMapper.java
+++ armor-api/src/main/java/com/zhixinhuixue/armor/dao/IZSYTaskMapper.java
@@ -4,10 +4,25 @@ import com.zhixinhuixue.armor.model.pojo.Task;
 
 public interface IZSYTaskMapper {
 
+    /**
+     * 根据主键删除
+     * @param id
+     * @return
+     */
     int deleteByPrimaryKey(Long id);
 
+    /**
+     * 插入任务
+     * @param record
+     * @return
+     */
     int insert(Task record);
 
+    /**
+     * 根据主键查询
+     * @param id
+     * @return
+     */
     Task selectByPrimaryKey(Long id);
 
 }
