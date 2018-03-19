3d27e70333ee47e0825d74a75f37f5c5bec7655b
Lang Zhe <491924704@qq.com>
2018-03-19 10:35:23 +0800

    计划

---
+++ armor-view/src/components/Plan.vue
@@ -0,0 +1,30 @@
+<template>
+</template>
+<script>
+    import http from '../lib/Http'
+    import moment from 'moment';
+    import ElButton from "../../node_modules/element-ui/packages/button/src/button";
+    import ElDialog from "../../node_modules/element-ui/packages/dialog/src/component";
+    import helper from '../lib/Helper'
+
+
+    moment.locale('zh-cn');
+    export default {
+        components: {
+            ElDialog,
+            ElButton},
+        name: 'Plan',
+        data() {
+            var validateEmpty = (rule, value, callback) => {
+                if (value.trim() == '') {
+                    callback(new Error());
+                } else {
+                    callback();
+                }
+            };
+            return {
+            }
+        },
+    }
+</script>
+
