3a84b11c9fc7e48a55f2703301a7e3196be83ef4
sch <schde163@163.com>
2019-11-19 15:21:00 +0800

    sql

--- armor-api/src/main/resources/sql/armor_20191114.sql
+++ armor-api/src/main/resources/sql/armor_20191114.sql
@@ -7,6 +7,10 @@ ADD COLUMN `bug_id`  bigint(20) NULL DEFAULT null COMMENT '线上任务bugId' AF
 ADD UNIQUE INDEX `task_user_unique` (`task_id`, `user_id`) USING BTREE COMMENT '任务-用户联合唯一索引',
 ADD UNIQUE INDEX `bug_user_unique` (`user_id`, `bug_id`) USING BTREE COMMENT 'bug-用户联合唯一索引';
 
+ALTER TABLE `task_log`
+MODIFY COLUMN `title`  varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '日志标题' AFTER `user_name`,
+MODIFY COLUMN `content`  text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '日志内容' AFTER `title`;
+
 
 
 
