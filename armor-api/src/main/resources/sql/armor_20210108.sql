e8e41795e899e1cb9a4236c43bd66277960c4bca
hesihai <1780606227@qq.com>
2021-01-13 14:51:28 +0800

    修改org_id 为 ordId

--- armor-api/src/main/resources/sql/armor_20210108.sql
+++ armor-api/src/main/resources/sql/armor_20210108.sql
@@ -8,124 +8,125 @@ CREATE TABLE if not exists `oranganization` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='机构';
 
+#1
 ALTER TABLE bug_manage ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#5
 ALTER TABLE bug_user ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#6
 ALTER TABLE department ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#7
 ALTER TABLE extra_work ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#8
 ALTER TABLE extra_work_task ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#9
 ALTER TABLE feedback ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#10
 ALTER TABLE feedback_likes ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#15
 ALTER TABLE feedback_plan ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#12
 ALTER TABLE feedback_queue ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#14
 ALTER TABLE feedback_read ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#13
 ALTER TABLE feedback_rejected ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#11
 ALTER TABLE feedback_reply ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#2
 ALTER TABLE mantis_bug_statistics ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#4
 ALTER TABLE mantis_category ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#3
 ALTER TABLE mantis_user ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#17
 ALTER TABLE notification ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#18
 ALTER TABLE online_question_record ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#19
 ALTER TABLE permission  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#20
 ALTER TABLE platform ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#21
 ALTER TABLE project ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#22
 ALTER TABLE publish_info ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#25
 ALTER TABLE resign_in ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#24
 ALTER TABLE sign_in ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#25
 ALTER TABLE tag ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#31
 ALTER TABLE task ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#26
 ALTER TABLE task_bug ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#27
 ALTER TABLE task_evaluation ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#28
 ALTER TABLE task_expand ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#29
 ALTER TABLE task_function ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#30
 ALTER TABLE task_log ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#31
 ALTER TABLE task_modify_function  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#32
 ALTER TABLE task_modify_table ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#33
 ALTER TABLE task_modify_user_week  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#34
 ALTER TABLE task_review ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#39
 ALTER TABLE task_review_log ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#35
 ALTER TABLE task_summary ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#36
 ALTER TABLE task_tag  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#38
 ALTER TABLE task_temp ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#37
 ALTER TABLE task_temp_function  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#16
 ALTER TABLE task_temp_module ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#40
 ALTER TABLE task_temp_tag  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#41
 ALTER TABLE task_test  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#42
 ALTER TABLE task_user ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#43
 ALTER TABLE test_example ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#44
 ALTER TABLE test_function ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#48
 ALTER TABLE `user` ADD ( org_id BIGINT (20) DEFAULT 1 COMMENT '机构ID', is_admin INT (1) DEFAULT 0 COMMENT '是否机构管理员:0:否，1:是' );
-
+#49
 ALTER TABLE user_check_people ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#45
 ALTER TABLE user_group ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#46
 ALTER TABLE user_integral ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#47
 ALTER TABLE user_leave ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#23
 ALTER TABLE user_rest_hours_log ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#24
 ALTER TABLE user_sort  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#48
 ALTER TABLE user_task_integral  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#49
 ALTER TABLE user_week ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#38
 ALTER TABLE user_week_temp ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#50
 ALTER TABLE week_publish_plan ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#51
 ALTER TABLE week_publish_plan_platform  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#52
 ALTER TABLE week_publish_plan_platform_user  ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#53
 ALTER TABLE week_publish_plan_task ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
-
+#54
 ALTER TABLE work_group ADD (org_id BIGINT (20) default 1 COMMENT '机构ID');
 
 
